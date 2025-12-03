ddaeb4fa2e9af7ee96b00b8f865dfb92f1060cf4
LewisMcI <77415321+LewisMcI@users.noreply.github.com>
2025-12-03 19:21:39 +0000

    Website Update

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // script.js
-window.addEventListener('load', ()=> {
+window.addEventListener('load', () => {
   const bg = document.querySelector('.hero .bg');
   const scroll = document.querySelector('.scrollone');
   const fg = document.querySelector('.hero .fg');
 
   // 1) Fade in background
-  setTimeout(()=> {
+  setTimeout(() => {
     bg.style.opacity = '1'
     scroll.style.transition = 'opacity 1s ease, transform 1s ease';
     scroll.style.opacity = '1';
     scroll.style.transform = 'translate(0%, -0%) scale(1)';
   }, 200);
   fg.style.transform = 'translate(0%, 5%) scale(1)';
-  
+
 
   // 2) Fade+move in foreground
-  setTimeout(()=>{
+  setTimeout(() => {
     fg.style.transition = 'opacity 1s ease, transform 1s ease';
     fg.style.opacity = '1';
     fg.style.transform = 'translate(0%, -0%) scale(1)';
